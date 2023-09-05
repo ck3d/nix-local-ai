9eadfc5bd310a2ec208d350e7e97f2767f61389c
Christian Kögler <ck3d@gmx.de>
2023-09-05 08:26:58 +0200

    chatbot-ui: Init 20230820

---
+++ chatbot-ui/default.nix
@@ -0,0 +1,40 @@
+{ mkYarnPackage
+, buildNpmPackage
+, fetchFromGitHub
+, lib
+, makeWrapper
+, inter
+}: buildNpmPackage rec {
+  pname = "chatbot-ui";
+  version = "20230820";
+
+  src = fetchFromGitHub {
+    owner = "mckaywrigley";
+    repo = pname;
+    rev = "138950c5520e80f69f059ecf0aea6a91a727cff9";
+    hash = "sha256-P0n9PXaJOFW4PH8crcQkfSR+dlYlt/5rEghddFPssxg=";
+  };
+
+  patches = [
+    ./font.patch
+  ];
+
+  npmDepsHash = "sha256-7mReAoIQcIk+n6UDYtLLlTyuT2F11jY9rvwJiykouVw=";
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  postConfigure = ''
+    cp ${inter}/share/fonts/opentype/Inter-Regular.otf pages
+  '';
+
+  postInstall = ''
+    rm -r .next/cache
+    rm -r $out/lib
+    mv -t $out \
+      .next public next.config.js next-i18next.config.js node_modules
+    makeWrapper $out/node_modules/.bin/next $out/${pname} \
+      --add-flags "start"
+  '';
+}
--- overlay.nix
+++ overlay.nix
@@ -6,5 +6,6 @@ in
   nix-local-ai = {
     local-ai = callPackage ./local-ai { };
     flowise = callPackage ./flowise { };
+    chatbot-ui = callPackage ./chatbot-ui { };
   };
 }
