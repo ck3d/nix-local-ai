1c118c6319e497b167511414be0458ddf1bc75d7
Christian Kögler <ck3d@gmx.de>
2024-03-17 17:48:59 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/5a415de6cf229de31ee927b774ccb20a4aa0f130' (2023-12-21)
      → 'github:nixos/nixpkgs/8ac30a39abc5ea67037dfbf090d6e89f187c6e50' (2024-03-16)
    • Added input 'nixpkgs-unstable':
        'github:nixos/nixpkgs/c75037bbf9093a2acb617804ee46320d6d1fea5a' (2024-03-16)

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   description = "Nix package of LocalAI";
 
-  # package go_1_21 is needed
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
     in
@@ -22,6 +24,18 @@
           in
           pkgs.nix-local-ai
           // { default = pkgs.nix-local-ai.local-ai; }
+        );
+
+      legacyPackages = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs-unstable {
+              inherit system;
+              overlays = builtins.attrValues overlays;
+              config = { allowUnfree = true; };
+            };
+          in
+          { unstable = pkgs.nix-local-ai; }
         );
     };
 }
