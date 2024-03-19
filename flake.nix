2b7375caabb214c6a8aea72779632368c29ac4c0
Christian Kögler <ck3d@gmx.de>
2024-03-19 07:35:33 +0100

    flake: Add checks

--- flake.nix
+++ flake.nix
@@ -9,9 +9,10 @@
   outputs = { self, nixpkgs, nixpkgs-unstable }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    rec {
       overlays.default = import ./overlay.nix;
+    in
+    {
+      inherit overlays;
 
       packages = forAllSystems
         (system:
@@ -19,7 +20,7 @@
             pkgs = import nixpkgs {
               inherit system;
               overlays = builtins.attrValues overlays;
-              config = { allowUnfree = true; };
+              config = { allowUnfree = true; allowBroken = true; };
             };
           in
           pkgs.nix-local-ai
@@ -32,10 +33,41 @@
             pkgs = import nixpkgs-unstable {
               inherit system;
               overlays = builtins.attrValues overlays;
-              config = { allowUnfree = true; };
+              config = { allowUnfree = true; allowBroken = true; };
             };
           in
           { unstable = pkgs.nix-local-ai; }
+        );
+
+      checks = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = builtins.attrValues overlays;
+              config = { allowUnfree = true; allowBroken = true; };
+            };
+            pkgs-unstable = import nixpkgs-unstable {
+              inherit system;
+              overlays = builtins.attrValues overlays;
+              config = { allowUnfree = true; allowBroken = true; };
+            };
+          in
+          {
+            inherit (pkgs.nix-local-ai.local-ai.passthru.tests) version health;
+          }
+          // builtins.listToAttrs
+            (map
+              (type: {
+                name = "health-" + type;
+                value = (pkgs-unstable.nix-local-ai.local-ai.override {
+                  "with_${type}" = true;
+                  with_stablediffusion = true;
+                  # tinydream can not compiled with cublas gcc
+                  with_tinydream = type != "cublas";
+                }).passthru.tests.health;
+              })
+              [ "cublas" "clblas" "openblas" ])
         );
     };
 }
