e212905fa46331c5ce26d7ed14482faade0f1c26
Christian Kögler <ck3d@gmx.de>
2024-04-21 19:46:13 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/d272ca50d1f7424fbfcd1e6f1c9e01d92f6da167' (2024-04-08)
      → 'github:nixos/nixpkgs/bc194f70731cc5d2b046a6c1b3b15f170f05999c' (2024-04-19)
    • Updated input 'nixpkgs-unstable':
        'github:nixos/nixpkgs/646f0dcd0133210c20805729e53168a15c7db211' (2024-04-11)
      → 'github:nixos/nixpkgs/5c24cf2f0a12ad855f444c30b2421d044120c66f' (2024-04-19)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable }:
