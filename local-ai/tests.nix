cb69c64926ee51c4b4d37e50163e815e93b789ef
Christian Kögler <ck3d@gmx.de>
2024-04-26 07:54:32 +0200

    local-ai: Remove version since cli has remove --version

--- local-ai/tests.nix
+++ local-ai/tests.nix
@@ -20,11 +20,6 @@ let
   inherit (self.lib) genModels;
 in
 {
-  version = testers.testVersion {
-    package = self;
-    version = "v" + self.version;
-  };
-
   health = testers.runNixOSTest ({ config, ... }: {
     name = self.name + "-health";
     nodes.machine = common-config;
