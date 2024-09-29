cb148d54e4cc0eed08a882779196e910899488ff
Christian Kögler <ck3d@gmx.de>
2024-09-29 21:39:55 +0200

    local-ai: 2.20.1 -> 2.21.1

--- flake.nix
+++ flake.nix
@@ -2,18 +2,16 @@
   description = "Nix package of LocalAI";
 
   inputs = {
-    nixpkgs-2405.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
 
   outputs =
     {
       self,
-      nixpkgs-2405,
       nixpkgs-unstable,
     }:
     let
-      inherit (nixpkgs-2405) lib;
+      inherit (nixpkgs-unstable) lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
@@ -24,13 +22,6 @@
       packages = forAllSystems (
         system:
         let
-          pkgs-2405 = import nixpkgs-2405 {
-            inherit system;
-            overlays = builtins.attrValues self.overlays;
-            config = {
-              allowUnfree = true;
-            };
-          };
           pkgs-unstable = import nixpkgs-unstable {
             inherit system;
             overlays = builtins.attrValues self.overlays;
@@ -40,9 +31,6 @@
           };
         in
         {
-          local-ai-nixos2405 = pkgs-2405.nix-local-ai.local-ai;
-          local-ai-openblas-nixos2405 = pkgs-2405.nix-local-ai.local-ai.override { with_openblas = true; };
-          local-ai-cublas-nixos2405 = pkgs-2405.nix-local-ai.local-ai.override { with_cublas = true; };
           default = pkgs-unstable.nix-local-ai.local-ai;
         }
         // pkgs-unstable.nix-local-ai
