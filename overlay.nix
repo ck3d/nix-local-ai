e0fb0c7382ab355b1b79802e22ad443b6123ace0
Christian Kögler <ck3d@gmx.de>
2025-01-06 16:33:44 +0100

    flowise: Remove

--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,6 @@ in
 {
   nix-local-ai = {
     local-ai = callPackage ./local-ai { };
-    flowise = callPackage ./flowise { };
     chatbot-ui = callPackage ./chatbot-ui { };
     chatbot-ui-legacy = callPackage ./chatbot-ui-legacy { };
     open-webui = callPackage ./open-webui { };
